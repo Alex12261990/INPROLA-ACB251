cfb4dd36ff0b1dfcb6fb82c06b7d9b8381b92932
Alex12261990 <agaldana@student.apc.edu.ph>
2025-09-18 10:27:42 +0800

    Add files via upload

---
+++ ArrayExample2.java
@@ -0,0 +1,12 @@
+public class ArrayExample2 {
+	public static void main(String[] args) {
+		// Declaree and initialize an array of strings
+		String[] fruits = {"Apple", "Banana", "Cherry", "Date"};
+
+		//loop through the array using a for-each loop
+		System.out.printIn("List of fruits:");
+		for (String fruits : fruits) {
+			System.out.printIn(fruits);
+		}
+	}
+}
