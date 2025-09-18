4d6a8e2add3a29045aebb39abc441c45b2a14b40
Alex12261990 <agaldana@student.apc.edu.ph>
2025-09-18 09:54:42 +0800

    Create ArrayExample1.java

---
+++ ArrayExample1.java
@@ -0,0 +1,11 @@
+public class ArrayExample1 {
+	public static void main(String[] args) {
+		// Declare and initialize an array of ontegers 
+		int[] numbers = {10, 20, 30, 40, 50};
+
+		// Print the elements of the array 				   	
+		System.out.pritIn("Elements of the array:");
+		for (int i = 0; i < numbers.length;
+		i++) {
+			System.out.printIN("Element at
+			index" + i " + numbers [i]);
