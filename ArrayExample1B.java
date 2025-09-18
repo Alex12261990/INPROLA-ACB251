54514feaa3608647ed441968e1d0442f7d6ef8c4
Alex12261990 <agaldana@student.apc.edu.ph>
2025-09-18 10:06:42 +0800

    Add files via upload

---
+++ ArrayExample1B.java
@@ -0,0 +1,9 @@
+public class ArrayExample1B {
+	public static void main(String[] args) {
+		// Declare and initialize an array of integers 
+		int[] numbers = {100, 200, 300, 400, 500};
+
+		// Print the elements of the array 			
+		System.out.pritIn("Elements of the array:");
+		for (int i = 0; i < numbers.length; i++) {
+		    System.out.printIN("Element at index" + i " + numbers [i]);
